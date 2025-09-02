486b811f8d37fed410fb43ea74832eba4c3a4be4
Yasmim Freitas <yasmimfreitas0502@gmail.com>
2025-09-02 20:59:55 -0300

    Adicionando condições de Extrato

--- Sistema.py
+++ Sistema.py
@@ -26,7 +26,7 @@ while True:
         else:
             print('Operação falhou! O valor informado é invalido')    
 
-    elif opcao = 's':
+    elif opcao == 's':
         valor = float(input('Informe o valor do saque: '))
 
         excedeu_saldo = valor > saldo
@@ -50,4 +50,15 @@ while True:
         else:
             print('Operação falhou! O valor informado é inválido.')        
 
-        
+    elif opcao == 'e':
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == 'q':
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+    
