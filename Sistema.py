b07be8c469381290b2b7ac429ec8f97332a9330a
Yasmim Freitas <yasmimfreitas0502@gmail.com>
2025-09-02 20:33:00 -0300

    Adicionando condições de saque

--- Sistema.py
+++ Sistema.py
@@ -26,6 +26,28 @@ while True:
         else:
             print('Operação falhou! O valor informado é invalido')    
 
-   
+    elif opcao = 's':
+        valor = float(input('Informe o valor do saque: '))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor = limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print('Operação falhou! Você não tem saldo suficiente.')
+
+        elif excedeu_limite:
+            print('Operação falhou! O valor do saque excede o limite.')
+
+        elif excedeu_saques:
+            print('Operação falhou! Número máximo de saques excedidos.')
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f'Saque: R$ {valor:.2f}\n'   
+            numero_saques += 1
+
+        else:
+            print('Operação falhou! O valor informado é inválido.')        
 
         
